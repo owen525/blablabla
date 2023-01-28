4ec1517fc60335f216d9479dd51a1186ed2b2e15
owen525 <119095241+owen525@users.noreply.github.com>
2023-01-28 11:18:24 +0700

    Add files via upload

---
+++ meeting 4/main.js
@@ -0,0 +1,12 @@
+score = prompt("masukan score")
+if (score > 80 - 100)  {
+    document.write ("A");
+} else if (65 - 79) {
+    document.write ("B");
+} else if (55 - 64) {
+    document.write ("C");
+} else if (50 - 54) {
+    document.write ("D");
+} else if (0 - 49) {
+    document.write ("E")
+}
